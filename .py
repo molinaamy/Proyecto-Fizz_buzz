8b35c53dc2d9131e9b72dfa8173b024c4d73859d
molinaamy <amymolina050607@gmail.com>
2023-05-02 11:55:19 -0600

    Proyecto Fizz Buzz

---
+++ .py
@@ -0,0 +1,15 @@
+def fizz_buzz(max_num):
+  for num in range(1, max_num + 1):
+    if num % 3 == 0 and num % 5 == 0:
+      print('FizzBuzz')
+    elif num % 5 == 0:
+      print('Buzz')
+    elif num % 3 == 0:
+      print('Fizz')
+    else:
+      print(num)
+
+
+fizz_buzz(100)
+  
+
